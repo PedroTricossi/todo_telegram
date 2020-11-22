73fea2802ef2525eba12a57c67dd5bc20d33aa4c
pdts20 <pdts20@inf.ufpr.br>
2020-11-21 22:22:14 -0300

    Remove more unecessary movies

--- DBConnect.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-
-def connect():
-    # Connect to an existing database
-    conn = psycopg2.connect(
-        host="localhost", dbname="todoTask", user="postgres", password="postgres")
-
-    return conn
